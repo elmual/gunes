80c13be4c712fca7fdecde6d48f63d5f8fb9f3ea
erlams <el@gmail.com>
2026-04-17 19:51:05 +0400

    yukle

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import './App.css'
+import Murad from './components/Murad'
 
 function App() {
 
   return (
     <>
       <h1>Leman</h1>
+      <Murad ad="Ahid" ixtisas="proqramci"/>
+      <Murad ad="Leman" ixtisas="Hekim"/>
+      <Murad ad="Gunes" ixtisas="Idmanci"/>
+      <Murad ad="Niko" ixtisas="Deputat"/>
     </>
   )
 }
---
+++ src/components/Murad.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Murad({ad, ixtisas}) {
+  return (
+    <div style={{border: 'solid 1px gray', padding: "10px", margin: "10px"}}>
+        <h1>Menim adim {ad}</h1>
+        <p>Men {ixtisas} isleyirem</p>
+    </div>
+  )
+}
+
+export default Murad
